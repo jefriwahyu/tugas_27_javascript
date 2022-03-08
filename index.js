e3ddd3f9a2df66939a3a4cabe15b479e35a31561
Jefriwahyu <memberjefry1@gmail.com>
2022-03-08 10:59:56 +0700

    Tugas 27 Javascript
    Tugas ini saya kumpulkan untuk NIOMIC.

---
+++ index.js
@@ -0,0 +1,13 @@
+function tugas27(){
+  var alamatRumah1 = "No ";
+  var alamatRumah2 = "rumah saya ";
+  var alamatRumah3 = "30 ";
+  var alamatRumah4 = "A ";
+  var alamatRumah5 = "Jalan pangeran Tirtayasa No ";
+  var alamatRumah6 = "70";
+
+  var hasil1 = alamatRumah1 + alamatRumah2 + alamatRumah3 + alamatRumah4 + alamatRumah5 + alamatRumah6;
+  console.log(hasil1);
+}
+
+tugas27();
