8a5ebc28870f69e02882ebe3d9c7db5bf3bbc403
jefriwahyu <92837751+jefriwahyu@users.noreply.github.com>
2022-08-13 16:28:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ function tugas27(){
   var alamatRumah5 = "Jalan pangeran Tirtayasa No ";
   var alamatRumah6 = "70";
 
-  var hasil1 = alamatRumah1 + alamatRumah2 + alamatRumah3 + alamatRumah4 + alamatRumah5 + alamatRumah6;
-  console.log(hasil1);
+  var hasil1 = alamatRumah1 + alamatRumah2 + alamatRumah3 + alamatRumah4;
+  var hasil2 =  alamatRumah5 + alamatRumah6;
+  
+  console.log(hasil1.concat(hasil2);
 }
 
 tugas27();
